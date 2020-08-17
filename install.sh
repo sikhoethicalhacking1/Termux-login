9eb655622dd0b603e5eaa374227f9b74ac003559
SIKHO ETHICAL HACKING <68183362+sikhoethicalhacking1@users.noreply.github.com>
2020-08-17 17:11:06 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+-do not copy this code-
+
+created by:- sikho ethical hacking
+
+gem install lolcat
+pkg install python
+pkg install requests
+pip2 install requests
+pkg install ruby 
+pkg install figlet  
+pkg install python2 
